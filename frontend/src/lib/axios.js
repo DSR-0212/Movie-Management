dc4e2f4a996ab412e4c444a06e05d26821fe0fe5
DSR-0212 <chmcs2026.sadaf.darukar@gmail.com>
2026-03-03 13:30:11 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    // baseURL: 'http://localhost:3000'
+    https://mo-5h62.onrender.com
 })
 export default api
